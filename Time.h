c11f4d52a7ef52076bb34a8cc3c8576d62602d00
rawiphonyai <rawiphon.yai@student.mahidol.edu>
2026-03-16 08:38:11 +0000

    Week 10

--- Time.h
+++ Time.h
@@ -2,6 +2,39 @@ struct Time{
 	int m,h,s;
 };
 
+void getTime(struct Time &t){
+    cout<<"Input Time(h m s)";
+    cin>>t.h>>t.m>>t.s;
+};
+
+struct Time subtract(struct Time t2 , struct Time t1){
+        struct Time t3; //t3=t2-t1
+        t3.s=t2.s-t1.s;
+        t3.m=t2.m-t1.m;
+        t3.h=t2.h-t1.h;
+
+        if(t3.s<0){
+            t3.m=t3.m-1;
+            t3.s=t3.s+60;
+        }
+ 
+        if(t3.m<0){
+            t3.h=t3.h-1;
+            t3.m=t3.m+60;
+        }
+
+        if(t3.h<0){
+            t3.h=t3.h+24;
+        }
+
+        return t3;
+};
+
+void display(struct Time t3){
+     cout<<setfill('0')<<setw(2)<<t3.h<<":";
+     cout<<setfill('0')<<setw(2)<<t3.m<<":";
+     cout<<setfill('0')<<setw(2)<<t3.s<<endl;
+};
 
 //create 3 functions
 
--- main.cpp
+++ main.cpp
@@ -5,13 +5,37 @@ using namespace std;
 
 #include "Time.h"
 
+
+
 int main(){
+
+    
     struct Time t1,t2,t3;
-    cout<<"What time was it?";
+    cout<<"What time was it? ";
+
     getTime(t1);
-    cout<<"What time is it now?";
+    cout<<"What time is it now? ";
+
     getTime(t2);
     t3=subtract(t2,t1); //t3=t2-t1
     cout<<"Time diff is ";
     display(t3);
+
+
+/*
+    int age;
+    string colour;
+    int *x =new int[5];
+
+    cout<<"Hello world"<<endl;
+    cout<<"Your age and colour : ";
+    cin>>age>>colour;
+    square(age);
+    cout<<"Age:"<<setfill('x')<<setw(8)<<fixed<<setprecision(2)<<age<<endl;
+    cout<<"Colour:"<<setfill('x')<<setw(8)<<colour<<endl;
+
+
+delete []x;
+ */
+
 }
